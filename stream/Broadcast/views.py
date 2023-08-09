49b749ac78b3a33db213987921f59a2c131d07ed
litslab <litslab@Dodo.litslab.net>
2023-08-09 07:14:50 +0100

    correction

--- stream/Broadcast/views.py
+++ stream/Broadcast/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from .models import Broadcast
 
-def liste_diffusions(request):
+def diffusion(request):
     diffusions = Broadcast.objects.all()
     context = {
         'diffusions': diffusions
